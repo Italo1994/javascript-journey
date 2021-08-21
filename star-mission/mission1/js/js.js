5cee44fbc04e0f96d67a663379f7bbae86933b4e
Italo1994 <italohortiz2015@gmail.com>
2021-08-21 04:04:20 -0300

    Commit of the first star mission

---
+++ star-mission/mission1/js/js.js
@@ -0,0 +1,9 @@
+function eventOnload() {
+	console.log("Onload is working!");
+}
+
+var missao = "Missão Estelar JS em execução!!!";
+
+function eventOnclick() {
+	console.log("Missão Estelar JS realizada com sucesso!!!");
+}
